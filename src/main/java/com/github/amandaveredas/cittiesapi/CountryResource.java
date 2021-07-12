5d75eab26da594432973bdc594a41c0224be1f07
amandaveredas <amandaveredas@gmail.com>
2021-07-12 14:53:11 -0300

    Pagination of countries and formatation of sql

--- src/main/java/com/github/amandaveredas/cittiesapi/CountryResource.java
+++ src/main/java/com/github/amandaveredas/cittiesapi/CountryResource.java
@@ -2,11 +2,11 @@ package com.github.amandaveredas.cittiesapi;
 
 import com.github.amandaveredas.cittiesapi.countries.Country;
 import com.github.amandaveredas.cittiesapi.repository.CountryRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/countries")
@@ -19,8 +19,9 @@ public class CountryResource {
     }
 
     @GetMapping
-    public List<Country> countries(){
-        return repository.findAll();
+    //Page: faz a paginação dos dados
+    public Page<Country> countries(Pageable page){
+        return repository.findAll(page);
 
     }
 }
