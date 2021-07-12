96e6bb97c429af3ee53a0b12fb82536d17bd79c3
amandaveredas <amandaveredas@gmail.com>
2021-07-12 15:16:29 -0300

    Get country by ID changed for Response Entity

--- src/main/java/com/github/amandaveredas/cittiesapi/CountryResource.java
+++ src/main/java/com/github/amandaveredas/cittiesapi/CountryResource.java
@@ -4,6 +4,7 @@ import com.github.amandaveredas.cittiesapi.countries.Country;
 import com.github.amandaveredas.cittiesapi.repository.CountryRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,9 +30,14 @@ public class CountryResource {
     }
 
     @GetMapping("/{id}")
-    public Country getOne(@PathVariable Long id) {
-        //refatorei como optional pois o id pode não existir
+    public ResponseEntity getOne(@PathVariable Long id) {
         Optional<Country> optional = repository.findById(id);
-        return optional.get();
+
+        if (optional.isPresent()){
+            return ResponseEntity.ok().body(optional.get());
+        } else{
+            return ResponseEntity.notFound().build();
+        }
+
     }
 }
